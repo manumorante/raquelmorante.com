a3cf7ca63e77b7c429d3f4c91ad8507c78e23acf
Manu Morante <manu.morante@domestika.org>
2020-03-30 11:24:40 +0200

    Gualine

---
+++ gualine/theme/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+?>
+
+<hr />
+<div id="footer" role="contentinfo">
+<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
+	<p>
+		<b><?php bloginfo('name'); ?></b> es un sitio web dise&ntilde;ado por <a href="http://www.unavezfui.com/">Manuel Morante</a> en la agencia <a href="http://www.estadologico.com/">Estado L&oacute;gico</a>
+		<br /><a href="/contacto/">Contacta con Gualine</a> | <a href="<?php bloginfo('rss2_url'); ?>">Suscribirse (RSS)</a>
+	</p>
+</div>
+</div>
+
+<!-- Gorgeous design by Michael Heilemann - http://binarybonsai.com/kubrick/ -->
+<?php /* "Just what do you think you're doing Dave?" */ ?>
+
+		<?php wp_footer(); ?>
+</body>
+</html>
